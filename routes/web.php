d4153e22c81975dcf2e29e8d0850580fd866d0bb
Yusaru7 <yusakou7@gmail.com>
2023-09-12 11:21:26 +0900

    ブログ投稿まで

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;      //追加
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+Route::get('/', [PostController::class, 'index']);  
 
+Route::get('/posts/create', [PostController::class, 'create']);
 
-Route::get('/posts', [PostController::class, 'index']);   
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::post('/posts', [PostController::class, 'store']);
+
+?>
